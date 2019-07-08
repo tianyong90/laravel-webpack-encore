07c91c1a51781326f2f0955ec3248edcbf68d595
tianyong90 <412039588@qq.com>
2019-07-09 00:42:11 +0800

    improve helpers

--- app/helpers.php
+++ app/helpers.php
@@ -4,16 +4,16 @@ use Illuminate\Support\HtmlString;
 
 /**
  * @param  string  $entryName
- * @return string
+ * @return HtmlString
  */
-function encore_entry_link_tags(string $entryName): string
+function encore_entry_link_tags(string $entryName): HtmlString
 {
     $entryPointsFile = public_path('js/entrypoints.json');
 
     $jsonResult = json_decode(file_get_contents($entryPointsFile), true);
 
     if (!array_key_exists('css', $jsonResult['entrypoints'][$entryName])) {
-        return '';
+        return null;
     }
 
     $tags = array_map(function ($item) {
@@ -25,16 +25,16 @@ function encore_entry_link_tags(string $entryName): string
 
 /**
  * @param  string  $entryName
- * @return string
+ * @return HtmlString
  */
-function encore_entry_script_tags(string $entryName): string
+function encore_entry_script_tags(string $entryName): HtmlString
 {
     $entryPointsFile = public_path('js/entrypoints.json');
 
     $jsonResult = json_decode(file_get_contents($entryPointsFile), true);
 
     if (!array_key_exists('js', $jsonResult['entrypoints'][$entryName])) {
-        return '';
+        return null;
     }
 
     $tags = array_map(function ($item) {
